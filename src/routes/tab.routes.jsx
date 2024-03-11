fc6ba4d731569d5afa720718384fb295d2273a33
Nickolas <nickolaslira.564@gmail.com>
2024-03-11 15:56:19 -0300

    Feat navigations, icons

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import Category from "../screens/Category/inex";
-
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Category" component={Category} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={"Black"} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={"Black"} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Category"
+        component={Category}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" color={"Black"} size={24} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -3,9 +3,22 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "pink",
+    backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "darkblue",
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
   },
 });
 
